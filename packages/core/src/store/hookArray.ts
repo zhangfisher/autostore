d48abaa390e76262c355564861a92a4fb3d9d132
zhangfisher <wxzhang@126.com>
2024-09-05 09:17:12 +0800

    update

--- packages/core/src/store/hookArray.ts
+++ packages/core/src/store/hookArray.ts
@@ -11,10 +11,18 @@ export function hookArrayMethods(notifyChange:any, array: any[], name:string,met
             }
             return result;
         };
+    }else if(name==='pop'){
+        return () => {
+            const oldLength = array.length;
+            const result = method.apply(array);
+            if (array.length == oldLength-1 ) {
+                notifyChange({type:'remove',path:parentPath,indexs:[oldLength-1],value:[result],oldValue:undefined, parentPath,parent:array});
+            }
+            return result;
+        }
     }else if(name==='splice'){
         return (start: number, deleteCount: number, ...items: any[]) => {
-            const deletedItems = deleteCount ===0 ?  [] : array.slice(start, start + deleteCount);
-            const result = method.apply(array, [start, deleteCount, ...items]);                
+            const deletedItems = method.apply(array, [start, deleteCount, ...items]);                
             if (deletedItems.length > 0) {
                 const deleteIndexs = Array.from({ length: deletedItems.length }, (_, i) => start + i);
                 notifyChange({type:'remove',path:parentPath,indexs:deleteIndexs, value:deletedItems,oldValue:undefined, parentPath,parent:array});
@@ -23,18 +31,46 @@ export function hookArrayMethods(notifyChange:any, array: any[], name:string,met
                 const addIndexs = Array.from({ length: items.length }, (_, i) => start + i);
                 notifyChange({type:'insert', path: parentPath,indexs:addIndexs,value: items,oldValue:undefined, parentPath,parent: array});
             }
-            return result;
+            return deletedItems;
         };
     }else if(name ==='unshift'){            
         return (...args: any[]) => {
             const oldLength = array.length;
             const result = method.apply(array, args);
             if (array.length > oldLength) {
-                const indexs = Array.from({ length: array.length - oldLength }, (_, i) => i);
-                notifyChange({type:'insert',path:parentPath,indexs,value: args,oldValue:undefined, parentPath,parent: array});
+                const addIndexs = Array.from({ length: array.length - oldLength }, (_, i) => i);
+                notifyChange({type:'insert',path:parentPath,addIndexs,value: args,oldValue:undefined, parentPath,parent: array});
             }
             return result;
         };
+    }else if(name==='shift'){
+        return () => {
+            const oldLength = array.length;
+            const result = method.apply(array);
+            if (array.length == oldLength-1 ) {
+                notifyChange({type:'remove',path:parentPath,indexs:[0],value:[result],oldValue:undefined, parentPath,parent:array});
+            }
+            return result;
+        }       
+    }else if(name==='fill'){
+        return (value: any, start?: number, end?: number) => { 
+            const result = method.apply(array, [value, start, end]);
+            // 计算受影响的索引
+            const startIndex = start ?? 0;
+            const endIndex = end ?? array.length;
+            const affectedIndexes = Array.from({ length: endIndex - startIndex }, (_, i) => i + startIndex);
+            const affectedValues = Array.from({ length: endIndex - startIndex }, () => value);
+            notifyChange({type:'update',path:parentPath,indexs:affectedIndexes,value:affectedValues,oldValue:undefined, parentPath,parent:array});
+            return result;
+        }
+    }else if(name==='concat'){
+        return (...items: any[]) => {
+            const oldLength = array.length; 
+            const result = method.apply(array, items);
+            const affectedIndexes = Array.from({ length: items.length }, (_, i) =>oldLength+i)
+            notifyChange({type:'insert',path:parentPath,indexs:affectedIndexes,value:items,oldValue:undefined, parentPath,parent:array});
+            return result;
+        }
     }else{
         return method
     }        
