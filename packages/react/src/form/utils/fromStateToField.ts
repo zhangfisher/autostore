9bb8bb1db55eb71e2721e3412b78d2c8ddabdab7
wxzhang <wxzhang@126.com>
2024-11-02 11:23:05 +0800

    update

--- packages/react/src/form/utils/fromStateToField.ts
+++ packages/react/src/form/utils/fromStateToField.ts
@@ -22,6 +22,20 @@ function defaultFromState(_:string,value:any,part:string | undefined){
     return value
 }
 
+function getStatePartValue(value:any,part:string | undefined){
+    if(!part) return value
+    if(Array.isArray(value) && isNumber(part)){
+        return value[part]
+    }else if(typeof(value) === "object"){
+        return value[part]
+    }else if(typeof(value)==='string'){ // 视为正则表达式，从字符串中提取
+        const matched = value.match(new RegExp(part))
+        if(matched){ // 如果有匹配组，则返回第一个匹配组
+            return matched.length===1 ? matched[0] : matched[1]   
+        } 
+    }
+    return value
+}
 
 /**
  * 将表单状态转换为字段值，并更新对应的表单元素。
@@ -35,6 +49,8 @@ export function fromStateToField(fieldInfo:AutoFormFieldContext,value:any,option
     const fromState = options.fromState || defaultFromState    
     let changed:boolean = false
     fieldInfo.inputs.forEach(input=>{
+        const part = input.dataset.fieldPart        
+        const newVal = fromState(fieldInfo.path,value,part)     
         if(initial){
             // 在初始化时，如果是checkbox或radio，则需要设置默认值
             if(input.type==='checkbox'){
@@ -53,11 +69,9 @@ export function fromStateToField(fieldInfo:AutoFormFieldContext,value:any,option
                     }
                 }
             }
-            input.dataset.typeof = Array.isArray(value) ? (value.length>0 ? typeof(value) : 'array') : typeof value
+            input.dataset.typeof = Array.isArray(newVal) ? 'array' : typeof newVal
         }
-        const part = input.dataset.fieldPart        
         const oldVal = getInputValue(input)
-        const newVal = fromState(fieldInfo.path,value,part)            
         if(initial || oldVal !== newVal){
             setInputValue(input,newVal)
             changed=true
