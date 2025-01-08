d2d55cac4ada3312516e205c6389b790b8561d01
wxzhang <wxzhang@126.com>
2025-01-08 17:49:44 +0800

    update

--- packages/syncer/src/index.ts
+++ packages/syncer/src/index.ts
@@ -3,14 +3,13 @@
  * import { Syncer } from '@autostore/sync';
 import { AutoStore } from '../../core/src/store/store';
  * 
- * const syncer = new AutoStoreSyncer([
- *  fromStore1,
- *  [
- *      tostore1,{},
- *      tostore2,{}
- *  ],
- *  
- * });
+ * const syncer = new AutoStoreSyncer(store);
+ * 
+ * 
+ * const store1 = syncer.fork(path1,{});
+ * const store2 = syncer.fork(path2,"");
+ * 
+ * 
  * 
  * 
  * - 将fromStore1的数据同步到tostore
@@ -24,16 +23,49 @@ import { AutoStore } from '../../core/src/store/store';
  * 
  */
 
+import { AutoStore, Dict, PATH_DELIMITER } from 'autostore';
+import { get as getByPath } from "flex-tools/object/get"
 
-export type SyncerOptions = {
+export type AutoStoreSyncerOptions = {
 
+} 
+
+export type AutoStoreForkOptions = {
+    entry? : string
+    sync?  : boolean        // copy, sync
+    filter?: (key:string,value:any)=>boolean
+} 
+
+
+export type AutoStoreFork = {
+    off():void
 }
 
-export type SyncToStore = [ AutoStore,SyncerOptions] 
 
-export class AutoStoreSyncer{
-    private _fromStore:AutoStore
-    constructor(fromStore:AutoStore,toStores:AutoStore | [AutoStore]){
-
+export class AutoStoreSyncer<State extends Dict = Dict>{
+    private _store:AutoStore<State>
+    private _forkStores:AutoStore<any>[] = []
+    private _options:AutoStoreSyncerOptions
+    constructor(store:AutoStore<State>,options?:AutoStoreSyncerOptions){
+        this._store = store;
+        this._options = Object.assign({},options);
     }
+    fork<State extends Dict = Dict>(options?:AutoStoreForkOptions):AutoStore<State>{
+        if(!this._store){
+            throw new Error("store is not initialized");
+        }
+        const { entry } = Object.assign({},options)
+
+
+        const snap = getByPath(this._store.getSnap(),entry || '');
+
+        const store = new AutoStore<State>(snap);
+        
+
+        this._forkStores.push(store);
+        return store;
+    }
+
+
+
 }
