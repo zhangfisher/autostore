6c8d30f8326e95dd9f8792e1239076367de30fd8
wxzhang <wxzhang@126.com>
2025-07-27 21:51:21 +0800

    update

--- packages/core/__tests__/reactive.test.ts
+++ packages/core/__tests__/reactive.test.ts
@@ -1,38 +1,69 @@
-import { describe,  expect, test } from 'vitest';
-import { AutoStore, isRaw,markRaw } from '../src';
-import { Dict } from 'flex-tools';
+import { describe, expect, test } from 'vitest';
+import { AutoStore, isRaw, markRaw } from '../src';
 
 // 测试 isEventMatched 函数
-describe("reactive",()=>{
-    test("use markRaw for object",()=>{
+describe('reactive', () => {
+    test('use markRaw for object', () => {
         const store = new AutoStore({
-            a:1,
-            b:2,
-            c:3,
-            total:async ()=>store.state.a+store.state.b+store.state.c,
-            x:markRaw({
-                total:async ()=>store.state.a+store.state.b+store.state.c,
-                x1:1,
-                x2:2,
-                x3:{
-                    x31:1,
-                    x32:2,
-                    x33: ()=>1
-                }
-            })
-        })
-        store.watch(({path})=>{
-            path  // ['a']
-        },{operates:'read'})  
-        expect(isRaw(store.state.a)).toBe(false)
-        expect(isRaw(store.state.x)).toBe(true)
-        let xxx = store.state.x.x1
-        store.state.x.x2        
-        store.state.x.x3.x31
-        store.state.x.x3.x32
-        store.state.x.x3.x33
-        store.state.x.total
-        store.state.total
-    })
-})
- 
+            a: 1,
+            b: 2,
+            c: 3,
+            total: async () => store.state.a + store.state.b + store.state.c,
+            x: markRaw({
+                total: async () => store.state.a + store.state.b + store.state.c,
+                x1: 1,
+                x2: 2,
+                x3: {
+                    x31: 1,
+                    x32: 2,
+                    x33: () => 1,
+                },
+            }),
+        });
+        store.watch(
+            ({ path }) => {
+                path; // ['a']
+            },
+            { operates: 'read' },
+        );
+        expect(isRaw(store.state.a)).toBe(false);
+        expect(isRaw(store.state.x)).toBe(true);
+        let xxx = store.state.x.x1;
+        store.state.x.x2;
+        store.state.x.x3.x31;
+        store.state.x.x3.x32;
+        store.state.x.x3.x33;
+        store.state.x.total;
+        store.state.total;
+    });
+    test('onForEachState', () => {
+        const store = new AutoStore(
+            {
+                user: {
+                    items: [
+                        {
+                            icon: 'settings',
+                            label: '设置',
+                            onClick: () => {
+                                console.log('settings');
+                            },
+                        },
+                        { icon: 'home', label: '首页', divider: true },
+                        { label: '仪表盘', badge: '1', divider: true },
+                    ],
+                },
+            },
+            {
+                onForEachState({ path, value }) {
+                    const name = path[path.length - 1];
+                    if (name && typeof value === 'function') {
+                        if (name.startsWith('render') || name.startsWith('on')) {
+                            markRaw(value);
+                        }
+                    }
+                },
+            },
+        );
+        store.state.user.items[0];
+    });
+});
